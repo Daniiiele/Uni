b0ad2475310dc5c3525307610360846844f5cc54
gh1r0o <118346766+gh1r0o@users.noreply.github.com>
2022-11-16 10:29:58 +0100

    Update Calendario anni bisestili.py

--- Calendario anni bisestili.py
+++ Calendario anni bisestili.py
@@ -1,21 +1,28 @@
-a=int(input("Anno:")) #immetti anno
+a = int(input("Anno:"))  #immetti anno
 #metodo A
-if(a%4 != 0):
-    print("Anno non Bisestile")
-elif(a%100!=0):
-    print("Anno Bisestile")
-elif(a%400==0):
-    print("Anno Bisestile")
+if (a % 4 != 0):
+  print("Anno non Bisestile")
+elif (a % 100 != 0):
+  print("Anno Bisestile")
+elif (a % 400 == 0):
+  print("Anno Bisestile")
 else:
-    print("Anno non Bisestile")
-#matodo B non funziona
-if a%4==0: #controlla divisibilita per4
-    if a%100!=0:
-        if a%400==0:
-             print("Anno non Bisestile")
-        else:
-            print("Anno 1Bisestile")
+  print("Anno non Bisestile")
+#metodo B
+if a % 4 == 0:  #controlla divisibilita per4
+  if a % 400 != 0:
+    if a % 100 == 0:
+      print("Anno non Bisestile")
     else:
-        print("Anno2 Bisestile")
+      print("Anno Bisestile")
+  else:
+    print("Anno Bisestile")
 else:
-    print("1Anno non Bisestile")
+  print("Anno non Bisestile")
+#metodo C
+if a % 4 == 0 and a % 400 == 0 or a % 100 != 0:
+  print("Anno bisestile")
+else:
+  print("Anno non bisestile")
+
+
